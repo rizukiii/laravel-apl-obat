053f98d77ee1b78a56a5c4461315c900e3a608a8
rizukiii <182581582+rizukiii@users.noreply.github.com>
2025-03-15 08:57:11 +0700

    create model migration seeder clasification & drug

---
+++ database/seeders/ClasificationSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClasificationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
